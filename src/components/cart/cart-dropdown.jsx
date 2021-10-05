16842c2727eac4439f816d93c9f3eaa8fe68b162
Deepak Jha3 <deepak.jha3@ibm.com>
2021-10-05 19:06:36 +0530

    Cart items sections added and utils function for the smae

--- src/components/cart/cart-dropdown.jsx
+++ src/components/cart/cart-dropdown.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 // //////////////////////////////////////
+// COMPONENTS
 import CustomButton from "components/custom-button/custom-button.jsx";
+import CartItem from "./cart-item";
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
 // //////////////////////////////////////
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, {})(CartDropdown);
