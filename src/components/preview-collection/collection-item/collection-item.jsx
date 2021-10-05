9a88f176f6528d9462facb59612231297a4bbfcc
Deepak Jha3 <deepak.jha3@ibm.com>
2021-10-05 18:26:19 +0530

    Added the new cart items and update the quantity logic

--- src/components/preview-collection/collection-item/collection-item.jsx
+++ src/components/preview-collection/collection-item/collection-item.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
 /////////////////////////////////////////
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+// ACTIONS
+import { addItemToCart } from "redux/actions";
+
+// COMPONENTS
+import CustomButton from "components/custom-button/custom-button";
+
+const CollectionItem = ({ item, addItem }) => {
+  const { id, name, price, imageUrl } = item;
+
   return (
     <div className="collection-item">
       <div
@@ -11,8 +20,17 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        ADD TO CART
+      </CustomButton>
     </div>
   );
 };
 /////////////////////////////////////////
-export default CollectionItem;
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItemToCart(item)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/actions.js
+++ src/redux/cart/actions.js
@@ -3,3 +3,13 @@ import { CartActionTypes } from "redux/actions";
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
 });
+
+export const addItemToCart = (product) => ({
+  type: CartActionTypes.ADD_ITEM_TO_CART,
+  payload: product,
+});
+
+export const removeItemFromCart = (id) => ({
+  type: CartActionTypes.REMOVE_ITEM_FROM_CART,
+  payload: id,
+});
